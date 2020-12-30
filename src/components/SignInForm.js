ff6a2dd9743ee6a118baa2d41f6cc41eb7d2d6b8
nizarhalloun <nshalloun@hotmail.com>
2020-12-30 15:06:25 +0200

    fix

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import axios from '../axios';
 
 const SignInForm = () => {
-  const { register, handleSubmit } = useForm();
+  const { register, errors, handleSubmit } = useForm();
   const onSubmit = async ({ email, password }, e) => {
     try {
       const {
@@ -26,9 +26,10 @@ const SignInForm = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="email" ref={register} />
-      <input name="password" ref={register} />
-
+      <input name="email" ref={register({ required: true })} />
+      {errors.email && 'Email is required'}
+      <input name="password" ref={register({ required: true })} />
+      {errors.password && 'Password is required'}
       <input type="submit" />
     </form>
   );
