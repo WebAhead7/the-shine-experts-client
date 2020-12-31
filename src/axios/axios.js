e5b2fee8e3ad378bb83eefc54a8f7bf949c2f154
nizarhalloun <nshalloun@hotmail.com>
2020-12-31 17:08:31 +0200

    env

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -6,8 +6,15 @@ if (token) {
   headers['x-auth-token'] = token;
 }
 
+let baseURL;
+if (process.env.NODE_ENV !== 'production') {
+  baseURL = process.env.REACT_APP_SERVER_URL_DEV;
+} else {
+  baseURL = process.env.REACT_APP_SERVER_URL;
+}
+
 export default axios.create({
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL,
   timeout: 1000,
   headers,
 });
